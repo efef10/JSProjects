7526fcadf30ae6d02f3416fd52e55689ce10326a
efef10 <co.efrat10@gmail.com>
2018-04-24 12:16:31 +0300

    choices

---
+++ day3/bits/index.js
@@ -0,0 +1,27 @@
+var optionMap = {
+    "humus":1,
+    "salat":2,
+    "hamutzim":4,
+    "chips":8
+}
+
+var choices = ["humus","salat","hamutzim","chips"]
+
+function is(obj, option){
+    if ((obj & optionMap[option]) == optionMap[option])
+        {
+            return true;
+        }
+        return false;
+}
+function set(obj,option){
+    return obj|optionMap[option];
+}
+
+var pita = Math.floor(Math.random()*4 + 1);
+var choice = choices[Math.floor(Math.random()*4)];
+
+console.log("search " + choice +" in: " + pita);
+console.log(is(pita,choice));
+if(!is(pita,choice))
+    console.log("after adding " + choice +": " + set(pita,choice));
